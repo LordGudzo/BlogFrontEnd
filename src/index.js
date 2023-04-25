6c67d316c96bee5a2a1bb9c357e0b43128555b74
LordGudzo <lordgudzo@ukr.net>
2023-04-25 11:35:18 +0300

    gets pasts and tags

--- src/index.js
+++ src/index.js
@@ -6,16 +6,20 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import  store  from "./redux/store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <div>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </div>
 );
